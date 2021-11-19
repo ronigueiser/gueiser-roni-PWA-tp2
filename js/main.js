d5f9d0fb38830ab1d5368fe3b873458ea3012436
ronigueiser <roni.gueiser@davinci.edu.ar>
2021-11-18 21:28:51 -0300

    Mejoras en el sitio, error al eliminar en Ver mas
    Todavia no se logro hacer que elimine la pelicula del local storage

--- js/main.js
+++ js/main.js
@@ -33,69 +33,94 @@ if (valorUltimaBusqueda != null){
     // drawMaker(valorUltimaBusqueda);
 }
 
+if (buttom != null) {
+    buttom.addEventListener('click', () => {
 
-buttom.addEventListener('click', () => {
 
+        // console.log(search.value);
 
-    // console.log(search.value);
+        fetch(`http://www.omdbapi.com/?t=${search.value}&apikey=${API_KEY}`
+        ).then(function (response) {
+            console.log(response);
+            return response.json();
+        }).then(function (responseJSON){
+            console.log('imprimo json', responseJSON);
+            drawMaker(responseJSON);
+            return responseJSON;
+        }).then((returnJSON) => agregarParaVerMasTarde(returnJSON))
+            .catch(function (error){
+                console.log('Fallo!',error)
+            });
 
-    fetch(`http://www.omdbapi.com/?t=${search.value}&apikey=${API_KEY}`
-    ).then(function (response) {
-        console.log(response);
-        return response.json();
-    }).then(function (responseJSON){
-        console.log('imprimo json', responseJSON);
-        drawMaker(responseJSON);
-        return responseJSON;
-    }).then((returnJSON) => agregarParaVerMasTarde(returnJSON))
-        .catch(function (error){
-        console.log('Fallo!',error)
-    });
+    })
+}
 
-})
 
 
 
 function drawMaker(data){
 
-    main.innerHTML = "";
+    console.log(data);
 
-    let divData = d.createElement('div');
-    divData.classList.add('container', 'card');
-    divData.style.width = '18rem';
-    main.appendChild(divData);
+    if (data.Response == 'False'){
 
-    let imgMovie = d.createElement('img');
-    imgMovie.src = data.Poster;
-    imgMovie.alt = 'Card image cap';
-    imgMovie.classList.add('card-img-top');
-    divData.appendChild(imgMovie);
+        main.innerHTML = "";
+        let divData = d.createElement('div');
+        divData.classList.add('container');
+        divData.style.width = '18rem';
+        main.appendChild(divData);
 
-    let divMovie = d.createElement('div');
-    divMovie.classList.add('card-body');
-    divData.appendChild(divMovie);
+        let pError = d.createElement('p');
+        pError.innerHTML = 'No se encontró ningún resultado';
+        pError.classList.add('error');
+        divData.appendChild(pError);
+    } else {
 
-    let h2Movie = d.createElement('h2');
-    h2Movie.innerHTML = data.Title;
-    divMovie.appendChild(h2Movie);
 
-    let pMovie = d.createElement('p');
-    pMovie.innerHTML = data.Plot;
-    divMovie.appendChild(pMovie);
 
-    let aMovie = d.createElement('a');
-    aMovie.innerHTML = 'Agregar';
-    aMovie.id = 'verMasTarde';
-    aMovie.href = '#';
-    aMovie.classList.add('btn', 'btn-primary');
-    divMovie.appendChild(aMovie);
+        main.innerHTML = "";
 
-    let aMovieDelete = d.createElement('a');
-    aMovieDelete.innerHTML = 'Quitar';
-    aMovieDelete.id = 'quitarDeLista';
-    aMovieDelete.classList.add('btn', 'btn-danger', 'btn-quitar');
-    aMovieDelete.href = '#';
-    divMovie.appendChild(aMovieDelete);
+        let divData = d.createElement('div');
+        divData.classList.add('container', 'card');
+        divData.style.width = '18rem';
+        main.appendChild(divData);
+
+        let imgMovie = d.createElement('img');
+        imgMovie.src = data.Poster;
+        imgMovie.alt = 'Card image cap';
+        imgMovie.classList.add('card-img-top');
+        divData.appendChild(imgMovie);
+
+        let divMovie = d.createElement('div');
+        divMovie.classList.add('card-body');
+        divData.appendChild(divMovie);
+
+        let h2Movie = d.createElement('h2');
+        h2Movie.innerHTML = data.Title;
+        h2Movie.classList.add('card-titulo');
+        divMovie.appendChild(h2Movie);
+
+        let pMovie = d.createElement('p');
+        pMovie.innerHTML = data.Plot;
+        pMovie.classList.add('sinopsis');
+        divMovie.appendChild(pMovie);
+
+        let aMovie = d.createElement('a');
+        aMovie.innerHTML = 'Agregar';
+        aMovie.id = 'verMasTarde';
+        aMovie.href = '#';
+        aMovie.classList.add('btn', 'btn-primary');
+        divMovie.appendChild(aMovie);
+
+        let aMovieDelete = d.createElement('a');
+        aMovieDelete.innerHTML = 'Quitar';
+        aMovieDelete.id = 'quitarDeLista';
+        aMovieDelete.classList.add('btn', 'btn-danger', 'btn-quitar');
+        aMovieDelete.href = '#';
+        divMovie.appendChild(aMovieDelete);
+    }
+
+
 }
 
 
@@ -116,6 +141,7 @@ function agregarParaVerMasTarde(data){
 
 
 const saveMovieToStorage = (data) => {
+    console.log(data)
     //read 'Respuesta API' from localStorage as list
     const list = JSON.parse(localStorage.getItem('Respuesta API'));
     //Check if list is null
@@ -139,26 +165,98 @@ const saveMovieToStorage = (data) => {
 
 }
 
-const deleteMovieFromStorage = (data) => {
-    //read 'Respuesta API' from localStorage as list
-    const list = JSON.parse(localStorage.getItem('Respuesta API'));
-    //Check if list is null
-    if (list == null) {
-        //if list is null, create a new list
-        const newList = [];
-        //add new movie to list
-        newList.push(data);
-        //save list to localStorage
-        localStorage.setItem('Respuesta API', JSON.stringify(newList));
+function deleteMovieFromStorage (data){
+    console.log(data)
+    //lee 'Respuesta API' del localStorage
+    const lista = JSON.parse(localStorage.getItem('Respuesta API'));
+    //se fija si es null
+    if (lista == null) {
+        //si es null, crea una nueva lista
+        const nuevaLista = [];
+        //agrega el nuevo elemento
+        nuevaLista.push(data);
+        //graba la lista en el localStorage
+        localStorage.setItem('Respuesta API', JSON.stringify(nuevaLista));
     } else {
-        //Check if data is in list
-        const isInList = list.some(item => item.Title === data.Title);
-        //If in list, remove it
+        //fija si el elemento esta en la lista
+        const isInList = lista.some(item => item.Title === data.Title);
+        //si esta, lo remueve
         if (isInList) {
-            let newList = list.filter(item => item.Title !== data.Title);
-            localStorage.setItem('Respuesta API', JSON.stringify(newList));
+            let nuevaLista = lista.filter(item => item.Title !== data.Title);
+            localStorage.setItem('Respuesta API', JSON.stringify(nuevaLista));
         }
 
     }
 
 }
+
+
+
+
+
+
+let mainVer = d.getElementById('ver-mas-tarde');
+
+//read what is in localStorage
+const listaVer = JSON.parse(localStorage.getItem('Respuesta API'));
+console.log('lista', listaVer);
+
+//recorrendo la lista de objetos
+function crearMostrador (){
+    for (let movies of listaVer){
+
+        let divData = d.createElement('div');
+        divData.classList.add('container', 'card');
+        divData.style.width = '18rem';
+        mainVer.appendChild(divData);
+
+        let imgMovie = d.createElement('img');
+        imgMovie.src = movies.Poster;
+        imgMovie.alt = 'Card image cap';
+        imgMovie.classList.add('card-img-top');
+        divData.appendChild(imgMovie);
+
+        let divMovie = d.createElement('div');
+        divMovie.classList.add('card-body');
+        divData.appendChild(divMovie);
+
+        let h2Movie = d.createElement('h2');
+        h2Movie.innerHTML = movies.Title;
+        h2Movie.classList.add('card-titulo');
+        divMovie.appendChild(h2Movie);
+
+        let pMovie = d.createElement('p');
+        pMovie.innerHTML = movies.Plot;
+        pMovie.classList.add('sinopsis');
+        divMovie.appendChild(pMovie);
+
+        // let aMovie = d.createElement('a');
+        // aMovie.innerHTML = 'Agregar';
+        // // aMovie.id = 'verMasTarde';
+        // aMovie.href = '#';
+        // aMovie.classList.add('btn', 'btn-primary',);
+        // divMovie.appendChild(aMovie);
+
+        let aMovieDelete = d.createElement('a');
+        aMovieDelete.innerHTML = 'Quitar';
+        // aMovieDelete.id = 'quitarDeListaVer';
+        aMovieDelete.classList.add('btn', 'btn-danger', 'btn-quitar', `${movies.Title}`, 'quitarDeListaVer');
+        aMovieDelete.href = '#';
+        divMovie.appendChild(aMovieDelete);
+
+    }
+}
+
+crearMostrador();
+
+let buttonQuitar = d.getElementsByClassName('quitarDeListaVer');
+console.log(buttonQuitar);
+
+for (let botonUnico of buttonQuitar){
+    console.log(botonUnico);
+}
+
+buttonQuitar.addEventListener('click', (e) => {
+
+    deleteMovieFromStorage(listaVer);
+})
