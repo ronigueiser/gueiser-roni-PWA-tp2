52c20abb5965edaff4bb44623e7506a34e7a95da
ronigueiser <roni.gueiser@davinci.edu.ar>
2021-11-19 21:02:41 -0300

    Pequeños detalles insignificantes

--- service-worker.js
+++ service-worker.js
@@ -1,16 +1,18 @@
-const CACHE = "version1";
+const CACHE = "version2";
 
-importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.0.0/workbox-sw.js');
+importScripts(
+    "https://storage.googleapis.com/workbox-cdn/releases/5.0.0/workbox-sw.js"
+);
 
 self.addEventListener("message", (event) => {
-  if (event.data && event.data.type === "SKIP_WAITING") {
-    self.skipWaiting();
-  }
+    if (event.data && event.data.type === "SKIP_WAITING") {
+        self.skipWaiting();
+    }
 });
 
 workbox.routing.registerRoute(
-    new RegExp('/*'),
+    new RegExp("/*"),
     new workbox.strategies.StaleWhileRevalidate({
-      cacheName: CACHE
+        cacheName: CACHE,
     })
-)
+);
